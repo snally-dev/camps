89d9f5179b24f3a86b25130b2470aac89f944016
cak <cak@typeerror.com>
2026-05-02 21:26:48 -0400

    feat: add initial configuration constants for the application

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const APP_NAME = "Camps";
+export const HOME_SCREEN_NAME = "Camps";
+export const CANONICAL_URL = "https://snally.com/camps/";
+export const MILESTONES = [50, 100, 150, 200, 250];
+export const MAIN_GOAL = 250;
